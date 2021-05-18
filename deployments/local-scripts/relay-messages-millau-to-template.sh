creator-chain/bridges

Add scripts for relaying Template<>Millau messages

deployments/local-scripts/relay-messages-millau-to-template.sh
@@ -1,0 +1,20 @@
+#!/bin/bash
+# A script for relaying Millau messages to the Template chain.
+#
+# Will not work unless both the Template and Millau are running (see `run-template-node.sh`
+# and `run-millau-node.sh).
+set -xeu
+
+MILLAU_PORT="${MILLAU_PORT:-9945}"
+TEMPLATE_PORT="${TEMPLATE_PORT:-9944}"
+
+RUST_LOG=bridge=debug \
+./target/debug/substrate-relay relay-messages MillauToTemplate \
+	--lane 00000000 \
+	--source-host localhost \
+	--source-port $MILLAU_PORT \
+	--source-signer //Bob \
+	--target-host localhost \
+	--target-port $TEMPLATE_PORT \
+	--target-signer //Bob \
+	--prometheus-host=0.0.0.0

deployments/local-scripts/relay-messages-template-to-millau.sh
@@ -1,0 +1,21 @@
+#!/bin/bash
+# A script for relaying Template messages to the Millau chain.
+#
+# Will not work unless both the Template and Millau are running (see `run-template-node.sh`
+# and `run-millau-node.sh).
+set -xeu
+
+MILLAU_PORT="${MILLAU_PORT:-9945}"
+TEMPLATE_PORT="${TEMPLATE_PORT:-9944}"
+
+RUST_LOG=bridge=debug \
+./target/debug/substrate-relay relay-messages TemplateToMillau \
+	--lane 00000000 \
+	--source-host localhost \
+	--source-port $TEMPLATE_PORT \
+	--source-signer //Bob \
+	--target-host localhost \
+	--target-port $MILLAU_PORT \
+	--target-signer //Bob \
+	--prometheus-host=0.0.0.0
+

xcaptain/parity-bridges-common

deployments/local-scripts/relay-messages-millau-to-template.sh
@@ -1,0 +1,20 @@
+#!/bin/bash
+# A script for relaying Millau messages to the Template chain.
+#
+# Will not work unless both the Template and Millau are running (see `run-template-node.sh`
+# and `run-millau-node.sh).
+set -xeu
+
+MILLAU_PORT="${MILLAU_PORT:-9945}"
+TEMPLATE_PORT="${TEMPLATE_PORT:-9944}"
+
+RUST_LOG=bridge=trace \
+./target/debug/substrate-relay relay-messages MillauToTemplate \
+	--lane 00000000 \
+	--source-host localhost \
+	--source-port $MILLAU_PORT \
+	--source-signer //Bob \
+	--target-host localhost \
+	--target-port $TEMPLATE_PORT \
+	--target-signer //Bob \
+	--prometheus-host=0.0.0.0

deployments/local-scripts/relay-messages-template-to-millau.sh
@@ -1,0 +1,21 @@
+#!/bin/bash
+# A script for relaying Template messages to the Millau chain.
+#
+# Will not work unless both the Template and Millau are running (see `run-template-node.sh`
+# and `run-millau-node.sh).
+set -xeu
+
+MILLAU_PORT="${MILLAU_PORT:-9945}"
+TEMPLATE_PORT="${TEMPLATE_PORT:-9944}"
+
+RUST_LOG=bridge=trace \
+./target/debug/substrate-relay relay-messages TemplateToMillau \
+	--lane 00000000 \
+	--source-host localhost \
+	--source-port $TEMPLATE_PORT \
+	--source-signer //Bob \
+	--target-host localhost \
+	--target-port $MILLAU_PORT \
+	--target-signer //Bob \
+	--prometheus-host=0.0.0.0
+